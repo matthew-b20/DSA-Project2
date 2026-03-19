2a4f6ff4c555e56b887a53618f66bee8f1a34333
cjsladek2 <cjsladek2@gmail.com>
2026-03-18 22:55:33 -0400

    Add epic flyTo effect to Searchbar

--- Frontend/react/src/App.tsx
+++ Frontend/react/src/App.tsx
@@ -1,15 +1,15 @@
-import Sidebar from  './components/Sidebar.tsx'
-import Legend from './components/Legend.tsx'
-import Searchbar from './components/Searchbar.tsx'
-import MapBackground from './components/MapBackground.tsx'
-
+import Sidebar from  './components/Sidebar.tsx';
+import Legend from './components/Legend.tsx';
+import Searchbar from './components/Searchbar.tsx';
+import { MapProvider } from 'react-map-gl/maplibre';
+import MapBackground from './components/MapBackground.tsx';
 export default function App() {
-  return (
-      <>
+    return (
+      <MapProvider>
           <MapBackground/>
           <Sidebar/>
           <Legend/>
           <Searchbar/>
-      </>
+      </MapProvider>
   )
 }
--- Frontend/react/src/components/MapBackground.tsx
+++ Frontend/react/src/components/MapBackground.tsx
@@ -1,14 +1,15 @@
-import Map from 'react-map-gl/maplibre'
-import 'maplibre-gl/dist/maplibre-gl.css'
-
+import Map from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
 export default function MapBackground() {
     return(
         <div className = "fixed top-0 left-0 w-screen h-screen fixed z-0">
             <Map
+            id="map"
+            attributionControl={false}
             initialViewState={{
                 longitude: -81.2,
                 latitude: 28.67,
-                zoom: 12
+                zoom: 12,
             }}
             className="w-full h-full"
             mapStyle="https://tiles.openfreemap.org/styles/liberty"/>
