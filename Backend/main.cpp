17839322cecf37da27e176ff6c7f30768c3fb218
cjsladek2 <cjsladek2@gmail.com>
2026-03-20 13:25:58 -0400

    Add DataCleaning folder to organize data files & move Crow to Backend & delete separate Dependencies folder

--- Backend/main.cpp
+++ Backend/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <chrono> // for timing of the methods
-#include "../Dependencies/Crow/include/crow.h"
+#include "./Crow/include/crow.h"
 using namespace std;
 using namespace chrono;
 
