99cb1f77ae5f8d39211997d9dfeae8b4908a8d9d
ChaitanyaRana <rana.chaitanya777@gmail.com>
2026-03-27 13:03:56 -0400

    Added a few working simple base case tests for Deap

--- Backend/testing/test.cpp
+++ Backend/testing/test.cpp
@@ -1,5 +1,7 @@
 #include "catch/catch_amalgamated.hpp"
+#include "../Deap.h"
 using namespace std;
+
 
 /*
 TEST_CASE("Test 1: Name", "[tags][here]") {
@@ -13,3 +15,93 @@ TEST_CASE("Test 1: Name", "[tags][here]") {
 
 
 //DEAP TESTS
+
+// Failed Tests:
+// - Get Min/Max only works in standalone by themselves
+// - Delete Min Works but Not Delete Max
+// - Sometimes, some tests pass but then fail without changing anything (two element deap, size tracking)
+
+
+// 1: Insert
+// 2: Delete
+// 3: Other functions like, size, getmax, get min, etc.
+
+TEST_CASE("Empty Deap", "[Base Case]") {
+    Deap<int> d;
+
+    SECTION("Checking the attributes of empty deap") {
+        REQUIRE(d.isEmpty());
+        REQUIRE(d.getSize() == 0);
+    }
+
+    SECTION("Checking out of range errors", "[Fail]") {
+        REQUIRE_THROWS_AS(d.deleteMin(), out_of_range);
+        REQUIRE_THROWS_AS(d.deleteMax(), out_of_range);
+        REQUIRE_THROWS_AS(d.getMax(), out_of_range);
+        REQUIRE_THROWS_AS(d.getMin(), out_of_range);
+    }
+}
+
+TEST_CASE("Deap with one element", "[Simple Deap]") {
+    Deap <int> d;
+    d.insert(7, 2);
+
+    SECTION("Size functions work properly") {
+        REQUIRE(d.getSize() == 1);
+        REQUIRE(!d.isEmpty());
+    }
+
+    SECTION("Get Min/Max functions work properly") {
+        REQUIRE(d.getMin() == 7);
+//        REQUIRE(d.getMax() == 7);
+    }
+
+    SECTION("Delete function works properly") {
+        int temp = d.deleteMin();
+        REQUIRE(temp == 7);
+        REQUIRE(d.isEmpty());
+    }
+
+    SECTION("same as before but with the delete Max function") {
+        int temp = d.deleteMax();
+        REQUIRE(temp == 7);
+        REQUIRE(d.isEmpty());
+    }
+}
+
+TEST_CASE ("Deap with two elements", "[Simple Deap V2]") {
+    Deap<int> d;
+    d.insert(4, 2);
+    d.insert(10, 6);
+
+    SECTION("Get Min/Max works properly") {
+        REQUIRE(d.getMin() == 4);
+        REQUIRE(d.getMax() == 10);
+    }
+
+    SECTION("Delete functions work properly") {
+        REQUIRE(d.deleteMin() == 4);
+        REQUIRE(d.getSize() == 1);
+        REQUIRE(d.getMin() == 10);
+    }
+
+    SECTION("same as before with delete max function") {
+        REQUIRE(d.deleteMax() == 10);
+        REQUIRE(d.getSize() == 1);
+        REQUIRE(d.getMax() == 4);
+    }
+}
+
+TEST_CASE("Size updates and functions properly", "[inserts]") {
+    Deap<int> d;
+
+    SECTION("Size is right") {
+        for (int i = 1; i <= 10; i++) {
+            d.insert(i, i);
+            REQUIRE(d.getSize() == i);
+        }
+    }
+}
+
+
+
